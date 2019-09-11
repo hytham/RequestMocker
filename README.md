55e6f5e0644564283d5d5c37313e423c3183e681
Hytham ALOBYDI <halobydi@rxsense.com>
2019-09-11 09:43:31 -0400

    Split the file and add to readme

--- README.md
+++ README.md
@@ -5,4 +5,13 @@ This is a simple package that will allow to mock the API endpoint at runtime.
 Just add the RequestMocker Package, map the expected response to end point and you are done
 
 # Example
-TBD
+The following example demonstrate  how to use this library
+1) After importing the Request Mocker library add the Mocker option line in the configuration service section of the startup file
+'''
+services.AddRequestMocker(x => {
+    // this represent an entry in the routing table
+	x.Map(HttpMethod.Get, "/sample", new { test = "test" });                  
+});
+'''
+2) Add the ''' app.UseRequestMocker(); ''' to the Configuration function as the first line. This is important since this will overtake any routing, if there is no match found in the routing table it will pass-it to the next part of the pipeline else it will intercept it and replay the assigned object with that rout
+
---
+++ RequestMocker/RequestMocker.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RequestMocker
+{
+    public class RequestMocker
+    {
+        private readonly RequestDelegate _next;
+        private readonly RequestMockerOptions _options;
+
+        public RequestMocker(RequestDelegate next, IOptions<RequestMockerOptions> options)
+        {
+            _next = next;
+            _options = options.Value;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var matchedRequest = TestRequest(httpContext, _options.mockedRouteingTable);
+            if (string.IsNullOrEmpty(matchedRequest))
+            {
+                await _next.Invoke(httpContext);
+            }
+            else
+            {
+                await httpContext.Response.WriteAsync(matchedRequest);
+            }
+        }
+
+        private string TestRequest(HttpContext context, Dictionary<string, Tuple<HttpMethod, object>> mockedRouteingTable)
+        {
+
+            var route = mockedRouteingTable.Keys.FirstOrDefault(x => x.Equals(context.Request.Path.Value.Trim().ToLower()));
+
+            if (route != null)
+            {
+                var responeBody = mockedRouteingTable[route];
+                context.Response.StatusCode = 200;
+                if (responeBody.Item1.ToString().ToLower() == context.Request.Method.ToLower())
+                {
+                    var responseString = JsonConvert.SerializeObject(responeBody.Item2);
+                    return responseString;
+                }
+                return string.Empty;
+
+            }
+            else return string.Empty;
+
+        }
+    }
+    public static class RequestMockerExtensions
+    {
+        public static IServiceCollection AddRequestMocker(this IServiceCollection service, Action<RequestMockerOptions> options)
+        {
+            options = options ?? (opts => { });
+            service.Configure(options);
+            return service;
+        }
+        public static IApplicationBuilder UseRequestMocker(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestMocker>();
+        }
+    }
+}
